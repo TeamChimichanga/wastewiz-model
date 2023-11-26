73e69bfa998728a67541588959bb95f2dd0c9d09
Martin Štrekelj <49723893+MartinStrekelj@users.noreply.github.com>
2023-11-26 08:43:56 +0100

    remove cors

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from roboflow import Roboflow
 from flask import Flask, request, jsonify
 import base64
 from dotenv import load_dotenv
-from flask_cors import CORS
 import os
 
 load_dotenv()
@@ -14,7 +13,6 @@ prediction_img_name = "prediction.jpg"
 temp_img_name = "temp.jpg"
 
 app = Flask(__name__)
-CORS(app)
 
 @app.route('/healtz', methods=['GET'])
 def healtz():
